e15acd281066b565058bf46a780aa1159a1078a2
aleksey <Aleksey.fgl@gmail.com>
2023-01-13 19:37:44 +0300

    add hw2

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
+import React, {FC} from 'react'
 import Affair from './affair/Affair'
-import {AffairType, FilterType} from '../HW2'
+import {AffairPriorityType, AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[] // need to fix any
+    setFilter: (value: FilterType) => void
+    deleteAffairCallback: (id:number) => void
     filter: FilterType
 }
 
-function Affairs(props: AffairsPropsType) {
-    const setAll = () => {
-        // need to fix
+function Affairs(props: AffairsPropsType): JSX.Element {
+
+    const setAll = (value: 'all'):void => {
+        props.setFilter(value)
     }
-    const setHigh = () => {
-        // need to fix
+    const setHigh = (value: 'high'):void => {
+        props.setFilter(value)
     }
-    const setMiddle = () => {
-        // need to fix
+    const setMiddle = (value: 'middle'):void => {
+        props.setFilter(value)
     }
-    const setLow = () => {
-        // need to fix
+    const setLow = (value: 'low'):void => {
+        props.setFilter(value)
     }
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
@@ -29,11 +30,11 @@ function Affairs(props: AffairsPropsType) {
     const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ? ' ' + s.active : '')
     const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
 
-    const mappedAffairs = props.data.map((a: AffairType) => (
+    const mappedAffairs: JSX.Element[] = props.data.map((a: AffairType) => (
         <Affair
             key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
             affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
+            deleteAffairCallback={() =>props.deleteAffairCallback(a._id)}
         />
     ))
 
@@ -42,28 +43,28 @@ function Affairs(props: AffairsPropsType) {
             <div className={s.buttonContainer}>
                 <button
                     id={'hw2-button-all'}
-                    onClick={setAll}
+                    onClick={() => setAll('all')}
                     className={cnAll}
                 >
                     All
                 </button>
                 <button
                     id={'hw2-button-high'}
-                    onClick={setHigh}
+                    onClick={() => setHigh('high')}
                     className={cnHigh}
                 >
                     High
                 </button>
                 <button
                     id={'hw2-button-middle'}
-                    onClick={setMiddle}
+                    onClick={() => setMiddle('middle')}
                     className={cnMiddle}
                 >
                     Middle
                 </button>
                 <button
                     id={'hw2-button-low'}
-                    onClick={setLow}
+                    onClick={() => setLow('low')}
                     className={cnLow}
                 >
                     Low
