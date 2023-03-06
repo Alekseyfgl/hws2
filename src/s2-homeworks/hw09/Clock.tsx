6748ea0e0f759e59b152ce0c302c723b8cc0555d
aleksey <aleksey.fgl@gmail.com>
2023-03-06 13:02:49 +0000

    add half hw9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,23 +1,50 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import {restoreState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
+import {logDOM} from "@testing-library/react";
+import {log} from "util";
+
+type DaysType = "Sunday" | "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday"
 
 function Clock() {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
+    const weekday: DaysType[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const timeCreator = (date: Date) => `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const getDay = (date: Date): DaysType => {
+        console.log()
+        return weekday[date.getDay()]
+    }
+
+
+    useEffect(() => {
+        //componentDidMount
+        //componentDidUpdate
+        //componentWillUnmount - перед удалением компонента
+        const timer = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        setTimerId(+timer)
+
+        return () => {
+            //componentWillUnmount
+            clearInterval(timerId)
+            setTimerId(undefined)
+        };
+    }, []);
+
 
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
     }
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
@@ -27,13 +54,13 @@ function Clock() {
 
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime = timeCreator(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
+    const stringDay = getDay(date) || <br/> // пишут студенты
     const stringMonth = 'date->month' || <br/> // пишут студенты
-
+    const isDisableTimer = !!timerId
     return (
         <div className={s.clock}>
             <div
@@ -66,14 +93,14 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={isDisableTimer} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={isDisableTimer} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
