0d9d91556356ac486b824976510225e1bd14ae7a
aleksey <aleksey.fgl@gmail.com>
2023-03-07 06:56:56 +0000

    add half hw9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -2,70 +2,68 @@ import React, {useEffect, useState} from 'react'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import {restoreState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
-import {logDOM} from "@testing-library/react";
-import {log} from "util";
+
 
 type DaysType = "Sunday" | "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday"
+type MonthType =
+    "January"
+    | "February"
+    | "March"
+    | "April"
+    | "May"
+    | "June"
+    | "July"
+    | "August"
+    | "September"
+    | "October"
+    | "November"
+    | "December"
 
 function Clock() {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
+    const month: MonthType[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const dayOfWeek: DaysType[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     const timeCreator = (date: Date): string => `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    const getDay = (date: Date): DaysType => dayOfWeek[date.getDay()]
-
-
-    useEffect(() => {
-        const delay: number = 1000;
-        //componentDidMount
-        //componentDidUpdate
-        //componentWillUnmount - перед удалением компонента
-
-        // const timer = setInterval(() => {
-        //     setDate(new Date());
-        // }, 1000);
-
-        let timer = setTimeout(function tick() {
-            setDate(new Date());
-            timer = setTimeout(tick, delay);
-        }, delay);
-
-        setTimerId(+timer)
-
-        return () => {
-            //componentWillUnmount
-            clearInterval(timerId)
-            setTimerId(undefined)
-        };
-    }, []);
+    const getDayTitle = (date: Date): DaysType => dayOfWeek[date.getDay()]
+    const getMonthTitle = (date: Date): MonthType => month[date.getMonth()]
 
 
     const start = () => {
+        const delay: number = 1000;
+        let timer = setTimeout(function tick() {
+            setDate(new Date());
+            timer = setTimeout(tick, delay);
+            setTimerId(+timer)
+        }, delay);
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
     }
 
     const stop = () => {
+        clearTimeout(timerId)
+        setTimerId(undefined)
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+        setShow(true)
     }
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+        setShow(false)
     }
 
+
     const stringTime = timeCreator(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${date.toLocaleDateString().replaceAll('/', ':')}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = getDay(date) || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
-    const isDisableTimer = !!timerId
+    const stringDay: DaysType = getDayTitle(date) || <br/> // пишут студенты
+    const stringMonth = getMonthTitle(date) || <br/> // пишут студенты
+
     return (
         <div className={s.clock}>
             <div
